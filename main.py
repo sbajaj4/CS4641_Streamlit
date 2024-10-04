6b2cad6dcdef13469ed4aea4df77acacb35c72cd
sbajaj4 <143217094+sbajaj4@users.noreply.github.com>
2024-10-04 00:36:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-csv = convert_df(df)
+csv = "customer_churn_data.csv"
 
 st.download_button(
     label="Download data as CSV",
