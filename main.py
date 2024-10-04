35df49e6ff99a7bfc6b778d9d6b83ee723154da4
sbajaj4 <143217094+sbajaj4@users.noreply.github.com>
2024-10-04 00:55:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ st.title('Proposal')
 st.header('Introduction')
 st.write('Customer churn is an important issue in the telecom industry, leading to revenue loss and additional costs to acquire new customers. To tackle this, companies have started predicting customer churn by identifying those at risk. This project will aim to develop a machine learning model that accurately predicts customer churn, minimizing profit loss and improving customer retention.')
 st.header('Background')
+st.subheader('Literature Review')
 st.write('The majority of the literature aims to predict whether a customer will churn. J. Bhattacharyya and M. K. Dash identified “ten overarching groups of scholarship” [1], the first two being churn prediction and modeling and feature selection techniques and comparison. Another paper proposes a six-step methodology starting with data pre-processing and feature analysis [2]. However, despite the extensive research and demand for churn reduction tools, there is a lack of “well-defined guidelines on appropriate model evaluation measures” [3].')
 st.write('There is a great demand for cross-industry model evaluation as well as a more generalized churn prediction model, as most are performed on one specific industry or consumer base [1]-[3].')
-st.subheader('Literature Review')
 st.subheader('Dataset Description')
+st.markdown(
+"""
+The dataset used is available on Kaggle and includes 1,000 samples with customer data:
+-CustomerID: Unique identifier.')
+-Age: Customer's age.')
+-Gender: Male/Female')
+-Tenure: Number of months with the service provider.')
+-MonthlyCharges: Monthly fees paid by the customer.')
+-ContractType: Month-to-Month, One-Year, or Two-Year.')
+-Churn: Target variable indicating whether the customer has churned (Yes/No).
+"""
+)
 st.header('Problem Definition')
 st.subheader('Problem')
 st.subheader('Motivation')
